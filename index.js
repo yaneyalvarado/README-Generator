10e0b7cf9581a77a86fdf9ae7eed65c6b324f01e
yaneyalvarado <alvyaney@gmail.com>
2023-04-19 08:20:23 -0600

    updated lines 4-5 & started the function to initialize app

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
+const fs = require("fs");
+const inquirer = require("inquirer");
+
 const questions = [
     {
         type: 'input',
@@ -50,6 +53,9 @@ function writeToFile(fileName, data) {}
 // TODO: Create a function to initialize app
 function init() {
     inquirer.prompt(questions)
+    .then((inquirerResponse, data) => {
+        console.log("Creating ReadMe");
+    })
 }
 
 // Function call to initialize app
