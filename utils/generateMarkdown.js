def225975c5e95d9174f386b6379cdd09ceb8dc2
yaneyalvarado <alvyaney@gmail.com>
2023-04-23 19:11:10 -0600

    updated generateMarkdown.js

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -13,16 +13,21 @@ function renderLicenseBadge(license) {
 function renderLicenseLink(license) {
   if (license) {
     return ``;
-  } else {
-    (license === 'MIT') {
-    return `https://img.shields.io/github/license/yaneyalvarado/README-Generator`
+  } else  if (license === 'MIT')  {
+  return `https://img.shields.io/github/license/yaneyalvarado/README-Generator`
   }
   }
-}
+
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (!license) {
+    return ``;
+  } else {
+    return  `https://img.shields.io/github/license/yaneyalvarado/README-Generator`
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
