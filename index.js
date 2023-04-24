b693dd260968acf39033651f8ab4b2e98bed1d19
yaneyalvarado <alvyaney@gmail.com>
2023-04-23 18:49:37 -0600

    updated readme

--- index.js
+++ index.js
@@ -10,37 +10,37 @@ const questions = [
         type: 'input',
         message: 'What is your project title?',
         name: 'Title'
-    }
+    },
     {
         type: 'input',
         message: 'Please describe your project',
         name: 'Description'
-    }
+    },
     {
         type: 'input',
         message: 'How can you install this project?',
         name: 'Installation'
-    }
+    },
     {
         type: 'input',
         message: 'How can the README GENERATOR be used?',
         name: 'Usage'
-    }
+    },
     {
         type: 'input',
         message: 'Who are the contributers?',
         name: 'Contributing'
-    }
+    },
     {
         type: 'input',
         message: 'Are there any tests?',
         name: 'Tests'
-    }
+    },
     {
         type: 'input',
         message: 'What is your GitHub username?',
         name: 'GitHub'
-    }
+    },
     {
         type: 'input',
         message: 'What is your email?',
@@ -49,12 +49,14 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(generateMarkdown, data) {}
+function writeToFile(generateMarkdown, data) {
+    
+}
 
 // TODO: Create a function to initialize app
 function init() {
     inquirer.prompt(questions)
-    .then((generateMarkdown, data) => {
+    .then((writeToFile, data) => {
         console.log("Creating ReadMe");
     })
 }
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,10 +1,24 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license) {
+    return ``;
+  } else {
+    return `(https://img.shields.io/github/license/yaneyalvarado/README-Generator)`
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license) {
+    return ``;
+  } else {
+    (license === 'MIT') {
+    return `https://img.shields.io/github/license/yaneyalvarado/README-Generator`
+  }
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
@@ -13,7 +27,20 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+ ${renderLicenseBadge(data.license)}
 
+ ## Table of Contents
+
+ ## Description
+
+ ## Installation
+
+ ## Usage
+${renderLicenseSection(data.license)}
+
+## Contributing
+
+## Tests
 `;
 }
 
